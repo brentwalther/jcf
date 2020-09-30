840f8ca8d6c24fe8391ec5addf8f3842f4e219a3
Brent Walther <brentwalther@gmail.com>
2020-09-30 18:24:48 -0500

    Rearrange match.sh arguments.

--- match.sh
+++ match.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-csv_file="$1.csv"
-ledger_file="$1.ledger"
-payee_transaction_mapping_file=$2
-ledger_account_listing=$3
+csv_file="$3.csv"
+ledger_file="$3.ledger"
+payee_transaction_mapping_file=$1
+ledger_account_listing=$2
 
 bazel run //:csv_matcher -- --tsv_desc_account_mapping $payee_transaction_mapping_file --ledger_account_listing $ledger_account_listing --transaction_csv $csv_file --output $ledger_file
 
